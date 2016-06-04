48411a51595a807e1f3d94053f7cbb568abdbf05
Edward Zeh <e.zeh@gmx.ch>
2016-06-04 15:25:51 +0200

    calling add_distance works now

--- raceanalysis/laps_compare2/scripts/add_distance_columns.js
+++ raceanalysis/laps_compare2/scripts/add_distance_columns.js
@@ -1,6 +1,8 @@
 
 
 define(['jquery'],function($) {
+
+	var fact_grad_m = 42000000/360;
 	
 	return function(in_array) {
 
@@ -11,8 +13,8 @@ define(['jquery'],function($) {
 		var lat_delta = 0.0;
 		var lon_delta = 0.0;
 		if (i>0) {
-			lat_delta = (lap1[i].lat - lap1[i-1].lat)*fact_grad_m;
-			lon_delta = (lap1[i].lon - lap1[i-1].lon)*fact_grad_m;	
+			lat_delta = (in_array[i].lat - in_array[i-1].lat)*fact_grad_m;
+			lon_delta = (in_array[i].lon - in_array[i-1].lon)*fact_grad_m;	
 
 			distance_to_previous_point = Math.sqrt(lat_delta*lat_delta + lon_delta*lon_delta);
 			distance_from_beginning += distance_to_previous_point;
--- raceanalysis/laps_compare2/scripts/main.js
+++ raceanalysis/laps_compare2/scripts/main.js
@@ -6,6 +6,9 @@ define(['./lap1','./lap2','./bla','./add_distance_columns'],function(lap1, lap2,
 console.log('hallo');
 console.log(lap1.length);
 console.log(lap2.length);
+
+var lap1_2 = add_distance_columns(lap1);
+var lap2_2 = add_distance_columns(lap2);
 	
 });
 
