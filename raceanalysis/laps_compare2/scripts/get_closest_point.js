b1e5f06cce21018498feaefef98443f43cad8f46
Edward Zeh <e.zeh@gmx.ch>
2016-06-04 15:50:17 +0200

    new module get_closest_point.js

--- raceanalysis/laps_compare2/scripts/get_closest_point.js
+++ raceanalysis/laps_compare2/scripts/get_closest_point.js
@@ -4,9 +4,16 @@ define(['jquery'],function($) {
 
 	return function(in_array,in_distance_from_beginning) {
 
+		var min_diff;
+		var min_diff_item;
+
+		$.each(in_array, function (i, o) {
+
 		// TODO
 
-		return 'bla';
+		});
+
+		return min_diff_item;
 	}	
 
 });
