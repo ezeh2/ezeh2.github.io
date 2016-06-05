c3121cc54e89032934587ac14a78ea18846b1141
Edward Zeh <e.zeh@gmx.ch>
2016-06-05 14:16:02 +0200

    correlate array function added

---
+++ raceanalysis/laps_compare2/scripts/correlate_arrays.js
@@ -0,0 +1,20 @@
+define(function() {
+
+    return function(in_array1, in_array2) {
+
+		var out_array = [];
+			
+		$.each(in_array1, function (i, o) {
+
+		  var in_array1_item = in_array1[i];
+		  var distance_from_beginning = in_array1_item.distance_from_beginning;
+
+		  var out_item = get_closest_point(in_array2, distance_from_beginning);
+		  out_array.push(out_item);
+
+		});
+
+    	return out_array;
+    };
+
+});
--- raceanalysis/laps_compare2/scripts/main.js
+++ raceanalysis/laps_compare2/scripts/main.js
@@ -1,24 +1,13 @@
 
 
 
-define(['./lap1','./lap2','./add_distance_columns','jquery','get_closest_point'],function(lap1,lap2,add_distance_columns,$, get_closest_point) {
+define(['./lap1','./lap2','./add_distance_columns','jquery','get_closest_point','correlate_arrays'],function(lap1,lap2,add_distance_columns,$, get_closest_point) {
 
 console.log('hallo');
 
 
 var lap1_2 = add_distance_columns(lap1);
 var lap2_2 = add_distance_columns(lap2);
-var lap3_2 = [];
-	
-$.each(lap1_2, function (i, o) {
-
-  var lap1_2_item = lap1_2[i];
-  var distance_from_beginning = lap1_2_item.distance_from_beginning;
-
-  var lap2_2_item = get_closest_point(lap2_2, distance_from_beginning);
-  lap3_2.push(lap2_2_item);
-
-});
 
 });
 
