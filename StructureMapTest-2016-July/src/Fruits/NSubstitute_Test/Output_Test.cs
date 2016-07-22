d759d6f246ba989ca7444bf846a2c780100c3443
Edward Zeh <Edward.Zeh@novartis.com>
2016-07-22 17:43:25 +0200

    nsubstitute added

---
+++ StructureMapTest-2016-July/src/Fruits/NSubstitute_Test/Output_Test.cs
@@ -0,0 +1,49 @@
+﻿using FruitInterfaces;
+using NSubstitute;
+using NSubstitute.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NSubstitute_Test
+{
+    public class Output_Test
+    {
+        [Test]
+        public void Test_Received()
+        {
+            // arrange
+            var output = Substitute.For<IOutput>();
+
+            // act
+            output.Print("3");
+            output.Print("1");
+            output.Print("9");
+
+            // assert
+            // 3 calls are saved
+            IEnumerable<ICall> calls = output.ReceivedCalls();
+            List<ICall> calls2 = calls.ToList();
+            Assert.That(calls2.Count, Is.EqualTo(3));
+
+            output.Received().Print("3");
+            output.Received().Print("1");
+            output.Received().Print("9");
+        }
+
+        [Test]
+        public void Test_Fruit_Weight()
+        {
+            // arrange
+            var output = Substitute.For<IFruit>();
+
+            // act
+            output.Weight.Returns(20);
+
+            // assert
+            Assert.That(output.Weight, Is.EqualTo(20));
+        }
+    }
+}
